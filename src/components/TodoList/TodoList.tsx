353fb9029a62d23f655cd82efa565b9ec1b9e0dd
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-02-25 20:21:04 +0600

    Add spinner

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -4,10 +4,12 @@ import './TodoList.css';
 import { AppDispatch, RootState } from '../../app/store';
 import { useEffect } from 'react';
 import { fetchTodoList } from './todoListThunks';
+import Spinner from '../Spinner/Spinner';
 
 const TodoList = () => {
   const dispatch: AppDispatch = useDispatch();
   const todos = useSelector((state: RootState) => state.todoList.items);
+  const isLoading = useSelector((state: RootState) => state.todoList.loading);
 
   useEffect(() => {
     dispatch(fetchTodoList());
@@ -19,14 +21,18 @@ const TodoList = () => {
         <input type='text' className='form-control' />
         <button className='btn btn-primary'>Save</button>
       </form>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          id={todo.id}
-          title={todo.title}
-          isDone={todo.isDone}
-        />
-      ))}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            isDone={todo.isDone}
+          />
+        ))
+      )}
     </div>
   );
 };
