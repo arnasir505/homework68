d5d96d2f07763e7a8458d015ee7d335fdf940f14
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-02-25 20:25:18 +0600

    Add logic to delete todo

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../app/store';
+import { deleteTodo, fetchTodoList } from '../TodoList/todoListThunks';
 
 interface Props {
-  id: string
-  title: string
-  isDone: boolean
+  id: string;
+  title: string;
+  isDone: boolean;
 }
 
-const TodoItem: React.FC<Props> = ({id, title, isDone}) => {
+const TodoItem: React.FC<Props> = ({ id, title, isDone }) => {
+  const dispatch: AppDispatch = useDispatch();
+  const onDelete = async (id: string) => {
+    await dispatch(deleteTodo(id));
+    await dispatch(fetchTodoList());
+  };
+
   return (
     <div className='card mb-2'>
       <div className='card-body d-flex align-items-center gap-3 fs-5'>
-        <input className='form-check-input m-0' type='checkbox' checked={isDone}/>
-        <p className='m-0'>{title}{id}</p>
-        <button type='button' className='btn-close ms-auto fs-6' />
+        <input
+          className='form-check-input m-0'
+          type='checkbox'
+          checked={isDone}
+        />
+        <p className='m-0'>{title}</p>
+        <button
+          type='button'
+          className='btn-close ms-auto fs-6'
+          onClick={() => onDelete(id)}
+        />
       </div>
     </div>
   );
--- src/components/TodoList/todoListThunks.ts
+++ src/components/TodoList/todoListThunks.ts
@@ -6,3 +6,10 @@ export const fetchTodoList = createAsyncThunk('todoList/fetch', async () => {
   const response = await axiosApi.get<ApiTodos | null>('/tasks.json');
   return response.data ?? {};
 });
+
+export const deleteTodo = createAsyncThunk(
+  'todoList/deleteTodo',
+  async (id: string) => {
+    await axiosApi.delete('/tasks/' + id + '.json');
+  }
+);
